5448964e6f08b4ea6e87140d67a0b10557c391a3
jsa3338 <heungnam3338@naver.com>
2025-07-15 18:18:31 +0900

    홈페이지 템플릿 공유

---
+++ 02_Website/bootstrap/flask_agriculture/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/blog-details')
+def blog_details():
+    return render_template('blog-details.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/testimonials')
+def testimonials():
+    return render_template('testimonials.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)

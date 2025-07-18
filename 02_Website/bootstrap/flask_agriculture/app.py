5ba3d5f2b9de1d9e13c2139d3aa7828cd1eb37c8
tangerineTaste <sim7178@naver.com>
2025-07-18 15:21:36 +0900

    차트 페이지 변경

--- 02_Website/bootstrap/flask_agriculture/app.py
+++ 02_Website/bootstrap/flask_agriculture/app.py
@@ -48,6 +48,9 @@ def signup():
 def find_password():
     return render_template('findPassword.html')
 
+@app.route('/test')
+def test():
+    return render_template('test.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
